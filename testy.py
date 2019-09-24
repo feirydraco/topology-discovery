cad8f332089a039f7a37cbbf3ca6f4b896d73c0d
feirydraco <vorayash97@gmail.com>
2019-09-24 22:38:49 +0530

    clean

--- testy.py
+++
@@ -1,7 +0,0 @@
-import timeit
-from timecomp import create_base
-from test import Skeleton
-g, ml=create_base(2)
-print(ml)
-Skeleton(g, ml)
-print(timeit.timeit(setup="from __main__ import create_base\nfrom test import Skeleton\ng, ml=create_base({})\nprint(ml)".format(4), stmt="Skeleton(g, ml)", number=1))
